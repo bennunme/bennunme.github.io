419f18f824e1fca7bdca9a49fe7f8e1ba5a1140c
EthanDevelops <66741050+EthanDevelops@users.noreply.github.com>
2020-11-13 10:57:54 -0500

    added https://en.wikipedia.org/wiki/Self-XSS

---
+++ assets/js/console.js
@@ -0,0 +1,17 @@
+
+var DEBUG = false;
+if(!DEBUG){
+    if(!window.console) window.console = {};
+    var methods = [ "debug", "warn", "info", "error"];
+    for(var i=0;i<methods.length;i++){
+        console[methods[i]] = function(){};
+    }
+}
+
+try
+{
+    var _0x44a1=["log","color: black; font-size: 18px;","color: red; font-size: 24px;","%cBy doing anthing in this console, you risk giving attackers access to your account or computer","%cWARNING","color: red; font-size: 75px; text-shadow: 2px 2px #000;"];!function(o,e){!function(e){for(;--e;)o.push(o.shift())}(++e)}(_0x44a1,343);var _0x30b5=function(o,e){return _0x44a1[o-=0]};setTimeout(function(){console.log(_0x30b5("0x3"),_0x30b5("0x4")),console.log("%cIf someone tells you to write, copy/paste or read anything from this console, there is a 101% chance you're being scammed! Read more on: https://en.wikipedia.org/wiki/Self-XSS",_0x30b5("0x0")),console[_0x30b5("0x5")](_0x30b5("0x2"),_0x30b5("0x0")),console[_0x30b5("0x5")]("%cNo, this console CANNOT be used to gain access to someone elses account, only yours. ",_0x30b5("0x0")),console[_0x30b5("0x5")]("%cStay safe and close this side-bar.",_0x30b5("0x1")),console[_0x30b5("0x5")]("%cThis is a tool of developers only.",_0x30b5("0x1"))},1500);
+}
+catch(e)
+{
+}
